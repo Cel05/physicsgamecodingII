8430aed849667ca1814f1eb3c3eb1caeb0361e36
Cel05 <ghan98@pratt.edu>
2026-05-16 15:18:01 -0400

    Revert "added color for pickup"
    This reverts commit 0e94a417aaf07378ea396974d4bd55a25733a1ce.

--- physicsgame_gamecodingII/Assets/Scripts/InteractibleObjects.cs
+++ physicsgame_gamecodingII/Assets/Scripts/InteractibleObjects.cs
@@ -2,52 +2,15 @@ using UnityEngine;
 
 public class InteractibleObjects : MonoBehaviour
 {
-    public Color highLightColor = new Color(1f, 0.95f, 0.6f);
-
-    [Range(0, 1f)] public float highLightStrangth = .4f;
-
-    private Renderer objectRenderer;
-    
-    private Color originalColor;
-
-    private bool isHighLighted = false;
-    
-    
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake()
+    void Start()
     {
-        objectRenderer = GetComponent<Renderer>();
-        if (objectRenderer != null)
-        {
-            originalColor = objectRenderer.sharedMaterial.color;
-        }
-
-        else
-        {
-            Debug.Log($"Interactable object on {gameObject.name} has no renderer. highLightwont work");
-        }
+        
     }
 
-    public void HighLight()
-
+    // Update is called once per frame
+    void Update()
     {
-        if (isHighLighted || objectRenderer == null)
-        {
-            Debug.Log("no obj renderer & ishighlighted is true");
-            return;
-        }
-
-        objectRenderer.material.color = Color.Lerp(originalColor, highLightColor, highLightStrangth);
-        isHighLighted = true;
+        
     }
-
-    public void UnhighLight()
-    {
-        if (!isHighLighted || objectRenderer == null) return;
-        objectRenderer.material.color = originalColor;
-        isHighLighted = false;
-    }
-
-  
 }
